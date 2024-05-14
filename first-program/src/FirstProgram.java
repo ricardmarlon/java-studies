1b088c2d2d7dffb6e486c4d6d640b68dc46a963b
ricardmarlon <mroricardo@gmail.com>
2024-05-14 10:51:42 -0300

    newbie java

---
+++ first-program/src/FirstProgram.java
@@ -0,0 +1,23 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        //This is how we can comment in Java
+        System.out.println("Hello World");
+
+        String nome = "Marlon";
+        int idade = 32;
+        String education = "MBA in Software Engineering";
+
+
+        System.out.print("My name's " + nome);
+        System.out.print(". I'm " + idade);
+        System.out.println(". I'm currently posgraduating in " + education + ".");
+
+
+
+
+
+
+
+
+    }
+}
