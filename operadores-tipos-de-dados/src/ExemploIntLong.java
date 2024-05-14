ee9ca3416c6dae2f88cbb1d4a3dc5436cfae0786
ricardmarlon <mroricardo@gmail.com>
2024-05-14 11:29:01 -0300

    Using operator and Data Types

---
+++ operadores-tipos-de-dados/src/ExemploIntLong.java
@@ -0,0 +1,9 @@
+public class ExemploInt {
+    public static void main(String[] args) {
+
+        //over Java 7 you can use underscore to improve the reading
+        int populacaoParaty = 43_680;
+
+        System.out.println(populacaoParaty);
+    }
+}
